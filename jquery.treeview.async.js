3ad5ae04179d72681e690ece75bb90123efb423f
Jörn Zaefferer <joern.zaefferer@gmail.com>
2013-02-07 10:52:08 +0100

    Update license to just MIT, like jQuery. Fixes #36
    Also removing trailing whitespace.

--- jquery.treeview.async.js
+++ jquery.treeview.async.js
@@ -3,14 +3,9 @@
  *
  * http://bassistance.de/jquery-plugins/jquery-plugin-treeview/
  *
- * Copyright (c) 2007 Jörn Zaefferer
- *
- * Dual licensed under the MIT and GPL licenses:
+ * Copyright 2010 Jörn Zaefferer
+ * Released under the MIT license:
  *   http://www.opensource.org/licenses/mit-license.php
- *   http://www.gnu.org/licenses/gpl.html
- *
- * Revision: $Id$
- *
  */
 
 ;(function($) {
--- jquery.treeview.js
+++ jquery.treeview.js
@@ -4,14 +4,9 @@
  * http://bassistance.de/jquery-plugins/jquery-plugin-treeview/
  * http://docs.jquery.com/Plugins/Treeview
  *
- * Copyright (c) 2007 Jörn Zaefferer
- *
- * Dual licensed under the MIT and GPL licenses:
+ * Copyright 2010 Jörn Zaefferer
+ * Released under the MIT license:
  *   http://www.opensource.org/licenses/mit-license.php
- *   http://www.gnu.org/licenses/gpl.html
- *
- * Revision: $Id: jquery.treeview.js 5759 2008-07-01 07:50:28Z joern.zaefferer $
- *
  */
 
 ;(function($) {
